ecc6f7ee9c63aa25e1c2a35e50165a6af7107497
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2025-03-18 06:04:23 +0200

    version2
    version2 changed target_col to target_cols which allow now add zero or more columns not just one and with same good performance and handle in arrays fragments not real data frame and update full dataframe at end of all loop and mapping and getting columns will added

--- map_rows_then_insert_col.py
+++ map_rows_then_insert_col.py
@@ -5,34 +5,21 @@ import pandas as pd
 it take 2 excel files add row for map and return excel file updated:
 print(map_rows_then_insert_col(updater, mapper, ['saymbols', 'saymbols'], 'price'))
 """
-file_to_update = {
-  "saymbols": ['ABB', 'ABB', 'ABB'],
-  "duration": [50, 40, 45]
-}
 
-# this the file have the new columns need to mapped to updater file rows or df (use list not process serios within loop or full df repaint +reflow, update df with df[]) 
-file_to_map = {
-  "saymbols": ['ABB', 'CDD', 'c'],
-  "price": [12.25, 1, 2]
-}
-
-#load data into a DataFrame object:
-updater = pd.DataFrame(file_to_update)
-
-mapper = pd.DataFrame(file_to_map)
 # order_col
 
 
 
 # this topest way to map and insert in performacne easy call (not change updater order get data for it)
-def map_rows_then_insert_col(updater, mapper, order_cols=[], target_col=''):
+def map_rows_then_insert_columns(updater, mapper, order_cols=[], target_cols=[]):
     updater = updater.copy()
     mapper = mapper.copy()
     np_nan = float('NaN')
-    new_col = []
+    new_columns = {}
     mappers = {}
-    if isinstance(updater, pd.DataFrame) and isinstance(mapper, pd.DataFrame) and isinstance(order_cols, list) and isinstance(target_col, str) and len(order_cols) == 2 and order_cols[0] in updater.columns and order_cols[1] in mapper.columns and target_col in mapper.columns:
- 
+
+    if isinstance(updater, pd.DataFrame) and isinstance(mapper, pd.DataFrame) and isinstance(order_cols, list) and isinstance(target_cols, list) and len(order_cols) == 2 and order_cols[0] in updater.columns and order_cols[1] in mapper.columns and all([True if target_col in mapper.columns else False for target_col in target_cols]):
+        
         for index, row in updater.iterrows():
             # here get update value by index
             col_val_in_updater = updater.loc[index, order_cols[0]]
@@ -54,22 +41,47 @@ def map_rows_then_insert_col(updater, mapper, order_cols=[], target_col=''):
                 else:
                     # this handle as i said map_index -1 so this handle the default 0 case first for any unique col
                     mappers[order_cols[0]] = 0
+                
+                
+                for target_col in target_cols:
+                    if target_col not in new_columns:
+                        new_columns[target_col] = []
                     
-                target_val = target_row_in_mapper.iloc[mappers[order_cols[0]]][target_col]
-                    
-                new_col.append(target_val)
+                    target_val = target_row_in_mapper.iloc[mappers[order_cols[0]]][target_col]
+                    new_columns[target_col].append(target_val)
                 #print(mappers[order_col], 'hi', '-'*30)
             else:
-                print('me not mapped', index)
-                new_col.append(np_nan)
-                continue
+                
+                for target_col in target_cols:
+                    if target_col not in new_columns:
+                        new_columns[target_col] = []
+                    new_columns[target_col].append(np_nan)
         # this can called in pandas the documentFragment invention and performance outsiden loop benfit and also (add lol typical as fragment the data to list not real excel or even real data frame so it hidden dataframe same as fragment but for python this part u need care all that for this
-        updater[target_col] = new_col
+        
+        for target_col, new_column in new_columns.items():
+            updater[target_col] = new_column
         return updater
     return None
 
 
-print(map_rows_then_insert_col(updater, mapper, ['saymbols', 'saymbols'], 'price'))
+file_to_update = {
+  "saymbols": ['ABB', 'ABB', 'ABB'],
+  "duration": [50, 40, 45]
+}
+
+# this the file have the new columns need to mapped to updater file rows or df (use list not process serios within loop or full df repaint +reflow, update df with df[]) 
+file_to_map = {
+  "saymbols": ['ABB', 'ABB', 'ABB'],
+  "price": [12.25, 1, 2],
+  "ok":['dynamic', 'true', 'add_row']
+}
+
+#load data into a DataFrame object:
+updater = pd.DataFrame(file_to_update)
+
+# add as many as needed and even make later callback
+mapper = pd.DataFrame(file_to_map)
+print(map_rows_then_insert_columns(updater, mapper, ['saymbols', 'saymbols'], ['price', 'ok']))
 
 print(updater)
 print(mapper)
